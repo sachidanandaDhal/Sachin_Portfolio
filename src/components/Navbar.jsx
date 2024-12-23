4217e9052d692d4a810094cfcace1a0ad7661c2f
sachidanandaDhal <dhal.sachidananda1@gmail.com>
2024-12-23 18:35:37 +0530

    `feat: updated package-lock.json, package.json, Navbar.jsx, index.css for new dependencies and styles`

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,52 @@
-
-import pic from '../../public/bubuPs.png';
+import pic from "../../public/bubuPs.png";
+import { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+import { IoCloseSharp } from "react-icons/io5";
 function Navbar() {
+  const [menu, setMenu] = useState(false);
+  const navItems = [
+    { id: 1, text: "Home" },
+    { id: 2, text: "About" },
+    { id: 3, text: "Portfolio" },
+    { id: 4, text: "Experiance" },
+    { id: 5, text: "Contact" },
+  ];
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 shadow-md">
-        <div className='flex justify-between items-center h-16'>
+      <div className="max-w-screen-2xl container mx-auto px-4  h-16 md:px-20 shadow-md fixed top-0 lef-0 right-0">
+        <div className="flex justify-between items-center h-16">
           <div className="flex space-x-2">
             <img src={pic} className="h-12 w-12 rounded-full" alt="" />
-            <h1 className='font-semibold text-xl cursor-pointer'>
-              Sachida<span className='text-green-500 text-2xl'>nanda</span>
-              <p className='text-sm'>Web Devloper</p>
+            <h1 className="font-semibold text-xl cursor-pointer">
+              Sachida<span className="text-green-500 text-2xl">nanda</span>
+              <p className="text-sm">Web Devloper</p>
             </h1>
           </div>
-          <div >
-            <ul className='flex space-x-8'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Portfolio</li>
-                <li>Experiance</li>
-                <li>Contacts</li>
+          {/* desktop navbar */}
+          <div>
+            <ul className="hidden md:flex space-x-8">
+              {navItems.map(({ id, text }) => (
+                <li  className="hover:scale-105 duration-200 cursor-pointer" key={id}>{text}</li>
+              ))}
             </ul>
+            <div onClick={() => setMenu(!menu)} className="md:hidden">
+              {menu ? <IoMenu size={24} /> : <IoCloseSharp size={24} />}
+            </div>
           </div>
         </div>
-        <div></div>
+        {/* mobile navbar */}
+        {menu && (
+          <div>
+            <ul className="md:hidden flex flex-col h-screen items-center justify-center space-y-3 text-xl">
+            {navItems.map(({ id, text }) => (
+                <li className="hover:scale-105 duration-200 font-semibold cursor-pointer" key={id}>{text}</li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
 }
 
-export default Navbar
+export default Navbar;
